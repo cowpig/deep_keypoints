9a526f1bd5e2f3cbe4097a882287acfc656c239d
Max <max@recoset.com>
2014-10-08 17:31:25 -0400

    bugfixes

--- hsnet.py
+++ hsnet.py
@@ -6,36 +6,47 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+batch_size = 64
+img_shape = (96,96)
+# bugcheck
+img_shape = (12,12)
+conv_filter_shape = (50, 1, 5, 5)
+conv_stride = (1,1)
+conv_poolsize = (2,2)
+
 floatX = theano.config.floatX
 data, labels = dm.shuffle_all(*dm.full_trainset())
 
 # data = np.array([img.ravel() for img in train], dtype=floatX)
 # labels = np.array([keymap.ravel() for keymap in labels], dtype=floatX)
 
-# quickly test new code for bugs
+# bugcheck
 data = np.array([img.ravel()[:12**2] for img in data], dtype=floatX)[:256]
 labels = np.array([keymap.ravel()[:12**2] for keymap in labels], dtype=floatX)[:256]
 
-n_train = len(data)
+n_train = int(len(data) * 0.7) - int(len(data) * 0.7) % batch_size
+n_test = int(len(data) - n_train) / 2
 
-s_train = theano.shared(data[:n_train], borrow=True)
-s_labels = theano.shared(labels[:n_train], borrow=True)
+trainx = theano.shared(data[:n_train], borrow=True)
+trainy = theano.shared(labels[:n_train], borrow=True)
+
+validx = theano.shared(data[n_train:n_train+n_test], borrow=True)
+validy = theano.shared(labels[n_train:n_train+n_test], borrow=True)
+testx = theano.shared(data[-n_test:], borrow=True)
+testy = theano.shared(labels[-n_test:], borrow=True)
 
 x = T.matrix('x')
 y = T.matrix('y')
 
-batch_size = 64
-img_shape = (96,96)
-# bugchecking
-img_shape = (12,12)
+
 dropout_pct = 0.3
 initial_input = x.reshape((batch_size,1) + img_shape)
 
 # given that memory is an issue, programmatically get slices of images
 
-conv_layer = conv.ConvLayer(inputs=initial_input, filter_shape=(50, 1, 4, 4),
-							image_shape=(batch_size, 1) + img_shape,
-							activation=theano.tensor.nnet.sigmoid, poolsize=(2, 2))
+conv_layer = conv.ConvLayer(inputs=initial_input, filter_shape=conv_filter_shape,
+							image_shape=(batch_size, 1) + img_shape, stride=conv_stride,
+							activation=theano.tensor.nnet.sigmoid, poolsize=conv_poolsize)
 framesize = 50*47*47
 # again for bugchecking
 framesize = 50*5*5
@@ -55,15 +66,22 @@ entropy = -T.sum(y * T.log(nn_layer.output) +
 cost = T.cast(T.mean(entropy), floatX)
 
 
-# needed for validation/testing
-# conv_layer_test = conv.ConvLayer(inputs=initial_input, filter_shape=(50, 1, 4, 4),
-# 							image_shape=(batch_size, 1) + img_shape,
-# 							activation=theano.tensor.nnet.sigmoid, poolsize=(2, 2))
-# nn_layer_test = nn.NNLayer(inputs=conv_to_nn, n_in=framesize, n_out=96*96, 
-# 						activation=theano.tensor.nnet.sigmoid)
+# needed for validation/testing.. 
+# TODO: I should refactor this into the conv/nnet classes
+convolutions = T.nnet.conv.conv2d(initial_input, conv_layer.W,
+				filter_shape=conv_filter_shape, image_shape=(n_test, 1) + img_shape,
+				subsample=conv_stride)
+pools = T.signal.downsample.max_pool_2d(input=convolutions, ds=conv_poolsize, ignore_border=False)
+conv_activation = T.nnet.sigmoid(pools + conv_layer.b.dimshuffle('x', 0, 'x', 'x'))
+conv_reshape = conv_activation.reshape((n_test, framesize))
+connected_output = T.nnet.sigmoid(T.dot(conv_reshape, nn_layer.W) + nn_layer.b)
+uncorrupted_entropy = -T.sum(y * T.log(connected_output) + 
+                        (1 - y) * T.log(1 - connected_output), axis=1)
+error = T.cast(T.mean(uncorrupted_entropy), floatX)
 
-
-the_trainer = trainer.Trainer([conv_layer, nn_layer], cost, x, s_train, y, s_labels)
+the_trainer = trainer.Trainer([conv_layer, nn_layer], cost, x, trainx, y, trainy,
+							valid_x=validx, valid_y=validy, test_x=testx, test_y=testy, 
+							error_func=error)
 func = the_trainer.get_training_function()
 
 the_trainer.run_epochs()
